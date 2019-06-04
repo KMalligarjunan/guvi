e3ba67811ee762bffe863ccb29d8ae5c17ef1bd6
KMalligarjunan <51353062+KMalligarjunan@users.noreply.github.com>
2019-06-04 00:19:07 -0700

    Update third.py

--- codes/third.py
+++ codes/third.py
@@ -1,6 +1,6 @@
 ch=input()
 if((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    if((ch==('a'or'e'or'i'or'o'or'u'or'A'or'E'or'I'or'O'or'U'))):
+    if((ch==('a'or'e'or'i'or'o'or'u')or('A'or'E'or'I'or'O'or'U'))):
         print("Vowel")
     else:
         print("Consonant")
