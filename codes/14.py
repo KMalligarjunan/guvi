8773ad14744c631b7e98c47ba91e90c09aed6ca5
KMalligarjunan <51353062+KMalligarjunan@users.noreply.github.com>
2019-06-07 20:11:55 +0530

    Update and rename 14.py to 15.py

--- codes/14.py
+++
@@ -1,5 +0,0 @@
-n,k=input().split()
-n,k=int(n),int(k)
-for i in range(n+1,k):
-	if(i%2!=0):
-		print(i,end=" ")
---
+++ codes/15.py
@@ -0,0 +1,5 @@
+n1,n2=input().split()
+n1,n2=int(n1),int(n2)
+for i in range(n1+1,n2):
+	if(i%2==0):
+		print(i,end=" ")
