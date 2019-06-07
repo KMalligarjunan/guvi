20595f2e6b6235b5166fb29c86d873f24da78629
KMalligarjunan <51353062+KMalligarjunan@users.noreply.github.com>
2019-06-07 19:44:39 +0530

    Update and rename Nine.py to 11.py

---
+++ codes/11.py
@@ -0,0 +1,4 @@
+N,k=input().split()
+N,k=int(N),int(k)
+pow=N**k
+print(pow)
--- codes/Nine.py
+++
@@ -1,9 +0,0 @@
-N,K=input().split()
-N,K=int(N),int(K)
-sum=0
-for i in range(1,N+1):
-	print(i,end=" ")
-for i in range(1,K+1):
-	sum+=i
-print()
-print(sum)
