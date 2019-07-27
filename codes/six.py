47b73db84b21452b9b317b915410432060f4310e
KMalligarjunan <51353062+KMalligarjunan@users.noreply.github.com>
2019-07-27 09:12:58 +0530

    Update six.py

--- codes/six.py
+++ codes/six.py
@@ -1,5 +1,12 @@
-y=int(input())
-if(y%4==0 and y%100!=0 or y%400==0):
-    print("yes")
-else:
-    print("no")
+N,Q=input().split()
+N,Q=int(N),int(Q)
+k=input().split()
+l=[]
+for i in range(0,Q):
+    s=input().split()
+    l.append(s)
+for i in range(0,Q):
+    count=0
+    for j in range(int(l[i][0])-1,int(l[i][1])):
+        count+=int(k[j])
+    print(count)
